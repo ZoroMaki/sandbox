ceea6149034823f62b08ee3c467f0aff555224e8
zoro <zoromaki>
2025-10-02 13:52:05 +0530

    Functions practice

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
 
@@ -81,41 +82,99 @@ public class App {
         // int count = 2;
 
         // while (count <= 7) { // instead of 7 we can take n & add scanner for that
-        //     int temp = b; // Must take temp as b, otherwise b value won't be correct one
-        //     b = b + a; // -> assigning b to addition of b+a
-        //     a = temp; // -> assigning to the temp value which we take first
-        //     count++;
+        // int temp = b; // Must take temp as b, otherwise b value won't be correct one
+        // b = b + a; // -> assigning b to addition of b+a
+        // a = temp; // -> assigning to the temp value which we take first
+        // count++;
         // }
         // System.out.println(b);//why b, because that's updated variable
-
 
         // int i=29429;
         // int count=0;
         // while(i>0){
-        //     int rem=i%10; //-> to pick last digit we are using modulas of 10
-        //     System.out.println(rem);
-        //     if(rem==9){
-        //         count++;
-        //     }
-        //     i/=10; //->reduce the digits one by one
-        //     System.out.println(i);
+        // int rem=i%10; //-> to pick last digit we are using modulas of 10
+        // System.out.println(rem);
+        // if(rem==9){
+        // count++;
+        // }
+        // i/=10; //->reduce the digits one by one
+        // System.out.println(i);
         // }
         // System.out.println(count);
-
 
         // int i=387763;
         // int ans=0;
         // while(i>0){
-        //     int rem = i%10; // gettting last digit
-        //     i/=10; // removing last digit from i
-        //     ans=(ans*10)+rem;
+        // int rem = i%10; // gettting last digit
+        // i/=10; // removing last digit from i
+        // ans=(ans*10)+rem;
         // }
         // System.out.println(ans);
 
         // Calculator try
 
+        // Switch
+        // Scanner sc = new Scanner(System.in);
+        // String fruits = sc.next();
+
+        // switch (fruits) {
+        // case "apple":
+        // System.out.println("Red healthy sugary");
+        // break;
+        // case "pomo":
+        // System.out.println("Granate with reddie juice");
+        // break;
+        // default:
+        // System.out.println("Something else");
+        // break;
+        // }
+
+        // int day = sc.nextInt();
+        // switch(day){
+        // case 1,2,3,4,5 -> System.out.println("weekday");
+        // case 6,7 -> System.out.println("Weekend");
+        // default -> System.out.println("Not date");
+        // }
+
         // List<Integer> lists= List.of(1,2,9,7,4,6);
         // lists.stream().filter(x->x%2==0).forEach(System.out::println);
 
+        // var result = isPrime(8);
+        // System.out.println(result);
+
+        System.out.println(isArmStrongNumber(153));
+        System.out.println(isArmStrongNumber(467));
+        System.out.println(isArmStrongNumber(977));
+
+        // for (int i = 100; i < 1000; i++) {
+        //     if (isArmStrongNumber(i)) {
+        //         System.out.println(i);
+        //     }
+        // }
+    }
+
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        int count = 2;
+        while (count * count <= num) {
+            if (num % count == 0) {
+                return false;
+            }
+            count++;
+        }
+        return count * count > num;
+    }
+
+    static boolean isArmStrongNumber(int num) {
+        int og = num;
+        int sum = 0;
+        while (og > sum) {
+            int rem = num % 10; // to pick last digit
+            num = num / 10;
+            sum = sum + rem * rem * rem;
+        }
+        return sum == og;
     }
 }
